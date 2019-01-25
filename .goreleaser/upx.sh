7e620991ef59ba740328b7d200727a23968348c4
bakito <github@bakito.ch>
2019-01-25 21:21:55 +0100

    no upx for darwin

--- .goreleaser/upx.sh
+++ .goreleaser/upx.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-upx -q dist/${1}*/*
+#upx -q dist/${1}*/*
+
+# ignore darwin; see https://github.com/upx/upx/issues/222
+find dist -type d -name "darwin*" -prune -o -type f -exec upx {} \;
+
